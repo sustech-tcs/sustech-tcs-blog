4917c3e898b1b7eb78ac4b64124856beeef9203b
Zhige Chen <zhigec_cpp@outlook.com>
2025-08-26 16:50:09 +0800

    Delete default posts and update readme.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TCS@SUSTech Blog
 
 This repository contains the source file of the official website of Theoretical Computer Science Club of Southern University of Science and Technology.
+
+The site is based on [saicaca/fuwari: ✨A static blog template built with Astro.](https://github.com/saicaca/fuwari)
--- src/config.ts
+++ src/config.ts
@@ -9,14 +9,14 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "TCS@SUSTech",
-	subtitle: "Veritas ex Ratione Symbolisque Floret",
+	subtitle: "",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
+		enable: true,
 		src: "assets/images/banner.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
